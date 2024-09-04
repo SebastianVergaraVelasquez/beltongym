08a5741140a893631268a4c21058333873f82374
SebastianVergaraVelasquez <97114145+SebastianVergaraVelasquez@users.noreply.github.com>
2024-09-04 11:21:36 -0500

    feat: :tada: Starting project

---
+++ src/main/java/com/sebastianvv/beltongym/BeltongymApplication.java
@@ -0,0 +1,13 @@
+package com.sebastianvv.beltongym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeltongymApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeltongymApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sebastianvv/beltongym/BeltongymApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sebastianvv.beltongym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeltongymApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
