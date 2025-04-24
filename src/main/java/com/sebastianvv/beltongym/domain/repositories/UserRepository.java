d8cc097f4842db38977047dd9320c1112d5dc067
SebastianVergaraVelasquez <97114145+SebastianVergaraVelasquez@users.noreply.github.com>
2025-04-24 15:47:50 -0500

    :fix: :recycle: Changes to fix Membership requests

--- src/main/java/com/sebastianvv/beltongym/domain/repositories/UserRepository.java
+++ src/main/java/com/sebastianvv/beltongym/domain/repositories/UserRepository.java
@@ -1,6 +1,7 @@
 package com.sebastianvv.beltongym.domain.repositories;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
@@ -10,4 +11,5 @@ import com.sebastianvv.beltongym.persistence.entities.User;
 public interface UserRepository extends CrudRepository<User, Integer> {
     @Query("SELECT u FROM User u JOIN FETCH u.userType")
     List<User> findAllWithUserType();
+    Optional<User> findByDocument(int document);
 }
--- src/main/java/com/sebastianvv/beltongym/web/controllers/MembershipController.java
+++ src/main/java/com/sebastianvv/beltongym/web/controllers/MembershipController.java
@@ -1,18 +1,26 @@
 package com.sebastianvv.beltongym.web.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.sebastianvv.beltongym.domain.services.membership.IMembership;
+import com.sebastianvv.beltongym.domain.services.user.IUser;
 import com.sebastianvv.beltongym.persistence.entities.Membership;
+import com.sebastianvv.beltongym.persistence.entities.User;
 
 import java.util.List;
+import java.util.Optional;
+
 
 @RestController
 @RequestMapping("/belton/membership")
 public class MembershipController {
     @Autowired
     private IMembership membershipService;
+    @Autowired
+    private IUser userService;
 
     @GetMapping
     public List<Membership> getAll() {
@@ -26,7 +34,43 @@ public class MembershipController {
 
     @PostMapping
     public Membership save(@RequestBody Membership membership) {
-        return membershipService.save(membership);
+        Optional<User> userOpt = userService.findByDocument(membership.getUser().getDocument());
+
+        if (userOpt.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND,
+                    "Usuario no encontrado con el documento: " + membership.getUser().getDocument());
+        }
+
+        Membership membership2 = new Membership();
+        membership2.setUser(userOpt.get());
+        membership2.setMembershipType(membership.getMembershipType());
+        membership2.setStartDate(membership.getStartDate());
+        membership2.setEndDate(membership.getEndDate());
+        membership2.setComment(membership.getComment());
+
+        return membershipService.save(membership2);
+    }
+
+    @PutMapping("/{id}")
+    public Membership put(@PathVariable int id, @RequestBody Membership updatedMembership) {
+        Optional<Membership> existingOpt = membershipService.findById(id);
+    if (existingOpt.isEmpty()) {
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Membresía no encontrada con id: " + id);
+    }
+
+    Optional<User> userOpt = userService.findByDocument(updatedMembership.getUser().getDocument());
+    if (userOpt.isEmpty()) {
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Usuario no encontrado con el documento: " + updatedMembership.getUser().getDocument());
+    }
+
+    Membership membership = existingOpt.get();
+    membership.setUser(userOpt.get());
+    membership.setMembershipType(updatedMembership.getMembershipType());
+    membership.setStartDate(updatedMembership.getStartDate());
+    membership.setEndDate(updatedMembership.getEndDate());
+    membership.setComment(updatedMembership.getComment());
+
+    return membershipService.save(membership);
     }
 
     @DeleteMapping("/{id}")
